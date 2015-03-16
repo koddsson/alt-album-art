e5382e3bada4da3284cc149689d69b200151fa95
Kristján Oddsson <koddsson@gmail.com>
2015-03-15 20:25:22 -0700

    Refactoring.

--- src/reddit_freshalbumart/core.cljs
+++ src/reddit_freshalbumart/core.cljs
@@ -10,10 +10,7 @@
   (:import [goog.net Jsonp]
            [goog Uri]))
 
-
 (def url "https://reddit.com/r/freshalbumart/hot.json?limit=100")
-
-(enable-console-print!)
 
 (defn jsonp [uri]
   "Make a GET request with a jsonp callback"
@@ -31,8 +28,6 @@
                       (close! ch)))))
     ch))
 
-(comment (go (js/alert (b64/encodeByteArray(<! (GET "https://i.imgur.com/GZkIfQQ.jpg"))))))
-
 "atom/localStorage"
 (defonce prefs (local-storage (atom {}) :prefs))
 
@@ -41,26 +36,30 @@
   (last (re-find #"(?im)(?:imgur\.com/)(.*)(?:.jpg|.png)$" url)))
 
 (defn filter-post [post]
+  "Return a boolean value indicating whether to include this post or not"
   (not (or 
     (not (get-imgur-id (:url post)))
-    (= (:thumbnail post) "nsfw")
     (= (:thumbnail post) "self"))))
 
 (defn render-image [{:keys [url permalink title]}]
-  "TODO: Use a HTML library"
-  (str "<a href='https://reddit.com/" permalink "' class='Image' style='background-image: url(" url ");'></div>"))
+  "Generate the HTML for a image"
+  (str "<a href='https://reddit.com/" permalink "'"
+       "class='Image' style='background-image: url(" url ");'></a>"))
 
 (defn render []
   "Renders urls as images in container"
-  (print (:invalidateTimestamp @prefs))
   (let [container (dom/getElementByClass "Container")]
-    (set! (.-innerHTML container) (apply str (map render-image (filter filter-post (:data @prefs)))))))
+    (set!
+      (.-innerHTML container)
+      (->> (:data @prefs)
+           (filter filter-post)
+           (map render-image)
+           (apply str)))))
 
 "Check the atom/localstorage for data and fetch if empty"
 (if (empty? @prefs)
   (go
-    (swap! prefs assoc :data (map :data (:children (:data (js->clj (<! (jsonp url)) :keywordize-keys true)))))
-    (swap! prefs assoc :invalidateTimestamp (now))))
+    (swap! prefs assoc :data (map :data (:children (:data (js->clj (<! (jsonp url)) :keywordize-keys true)))))))
 
 "Re-render when the atom is updated"
 (add-watch prefs :new (fn [_ _ _ ns] (render))) 
